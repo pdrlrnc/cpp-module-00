13c9a3a26f9db2168c97c1ce5dc53c38c1447da2
pdrlrnc <pedrolourencoemail@gmail.com>
2026-01-30 16:14:22 +0000

    now it looks more like cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <cctype>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	return (0);
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else 
+	{
+		for (int i = 1; argv[i]; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+	}
+	std::cout << std::endl;
 }
